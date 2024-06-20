56785bf3835ba51bcdb7570310dcba1ebb5d62d2
Nassif6758 <nassiftheprogrammer@gmail.com>
2024-06-20 12:04:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 const entry = document.getElementById("entry");
 const know = document.getElementById("know");
-const entries = document.getElementById("entries"); // Move entries outside the functions to make it accessible globally
+const entries = document.getElementById("entries");
 
 entry.onclick = function newentry() {
     const data = document.createElement("div");
+    data.classList.add("entry");  // Add a class for easier styling and selection
     data.innerHTML = `<h3>New device</h3>
-    <input id="energy" placeholder="Number of watts"><br><br>
-    <input id="number" placeholder="Number of devices"><br><br>
-    <input id="time" placeholder="Number of hours used"><br><br>
-    <input id="unit" placeholder="Bill per unit"><br><br>
-    <input id="months" placeholder="Dates of the current month"><br><br>`;
+    <input class="energy" placeholder="Number of watts"><br><br>
+    <input class="number" placeholder="Number of devices"><br><br>
+    <input class="time" placeholder="Number of hours used"><br><br>
+    <input class="unit" placeholder="Bill per unit"><br><br>
+    <input class="months" placeholder="Dates of the current month"><br><br>
+    <button class="delete">Delete</button><br><br>`;
     entries.appendChild(data);
+
+    // Add event listener to the delete button
+    data.querySelector(".delete").onclick = function() {
+        entries.removeChild(data);
+    };
 }
 
 know.onclick = function calculatedbill() {
     let totalbill = 0;
-    const allData = document.querySelectorAll("#entries div"); // Select all dynamically created divs inside #entries
+    const allData = document.querySelectorAll("#entries .entry");
     allData.forEach(info => {
-        const energy = info.querySelector("#energy").value;
-        const time = info.querySelector('#time').value;
-        const number = info.querySelector('#number').value;
-        const unit = info.querySelector('#unit').value;
-        const months = info.querySelector('#months').value;
-        const electricitybill= energy * time * number * unit * months / 1000;
-        totalbill += electricitybill
+        const energy = info.querySelector(".energy").value;
+        const time = info.querySelector('.time').value;
+        const number = info.querySelector('.number').value;
+        const unit = info.querySelector('.unit').value;
+        const months = info.querySelector('.months').value;
+        const electricitybill = energy * time * number * unit * months / 1000;
+        totalbill += electricitybill;
     });
-    result.innerText = `The calculated bill is ${totalbill}`;
+    document.getElementById("result").innerText = `The calculated bill is ${totalbill}`;
 }
